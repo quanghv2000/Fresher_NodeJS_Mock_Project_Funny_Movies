87eb7d1bb5dd52cf0ef9c1cfbea761094fb2c5bc
quanghv2000 <quanghv2000.dev@gmail.com>
2022-07-15 01:05:03 +0700

    Create Authentication with JWT and Authorization

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,66 +1,101 @@
 /* eslint-disable prettier/prettier */
-import { Body, Controller, Delete, Get, HttpException, HttpStatus, Param, ParseIntPipe, Post, Put, Req } from "@nestjs/common";
-import { Request } from "express";
-import { UserDTO } from "../services/dtos/user.dto";
-import { UserService } from "../services/user.service";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+  Req,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { Request } from 'express';
+import { Roles } from '../security/decorators/roles.decorator';
+import { RoleType } from '../security/role-type';
+import { UserDTO } from '../services/dtos/user.dto';
+import { UserService } from '../services/user.service';
+import { RolesGuard } from '../security/guards/roles.guard';
+import { AuthGuard } from '../security/guards/auth.guard';
 
 @Controller('user')
 export class UserController {
-    constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) {}
 
-    
-    @Get('/get-all')
-    async getAllCategories(): Promise<UserDTO[]> {
-        const users = await this.userService.findAll();
+  @Roles(RoleType.ADMIN)
+  @UseGuards(AuthGuard, RolesGuard)
+  @Get('/get-all')
+  async getAllCategories(): Promise<UserDTO[]> {
+    const users = await this.userService.findAll();
 
-        return users;
+    return users;
+  }
+
+  @Roles(RoleType.ADMIN)
+  @UseGuards(AuthGuard, RolesGuard)
+  @Get('/get-by-id/:id')
+  async getUserById(@Param('id', ParseIntPipe) id: number): Promise<UserDTO> {
+    const userFound = await this.userService.findById(id);
+
+    if (!userFound) {
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
     }
 
-    @Get('/get-by-id/:id')
-    async getUserById(@Param('id', ParseIntPipe) id: number): Promise<UserDTO> {
-        const userFound = await this.userService.findById(id);
+    return userFound;
+  }
 
-        if (!userFound) {
-            throw new HttpException('User not found', HttpStatus.NOT_FOUND);
-        }
+  @Roles(RoleType.ADMIN)
+  @UseGuards(AuthGuard, RolesGuard)
+  @UsePipes(ValidationPipe)
+  @Post('/create')
+  async createUser(
+    @Req() req: Request,
+    @Body() userDTO: UserDTO,
+  ): Promise<UserDTO> {
+    const userReq: any = req.user;
+    userDTO.createdBy = userReq.email;
+    const userCreated = await this.userService.save(userDTO);
 
-        return userFound;
+    return userCreated;
+  }
+
+  @Roles(RoleType.ADMIN)
+  @UseGuards(AuthGuard, RolesGuard)
+  @UsePipes(ValidationPipe)
+  @Put('/update')
+  async updateUser(
+    @Req() req: Request,
+    @Body() userDTO: UserDTO,
+  ): Promise<UserDTO> {
+    const userFoundById = await this.userService.findById(userDTO.id);
+
+    if (!userFoundById) {
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
     }
 
-    @Post('/create')
-    async createUser(@Req() req: Request, @Body() userDTO: UserDTO): Promise<UserDTO> {
-        // const userReq = req.user?.login;
-        // userDTO.createdBy = userReq;
-        const userCreated = await this.userService.save(userDTO);
+    const userReq: any = req.user;
+    userDTO.createdBy = userReq.email;
 
-        return userCreated;
+    const userUpdated = await this.userService.update(userDTO);
+
+    return userUpdated;
+  }
+
+  @Roles(RoleType.ADMIN)
+  @UseGuards(AuthGuard, RolesGuard)
+  @Delete('/delete/:id')
+  async deleteUser(@Param('id') id: number): Promise<UserDTO> {
+    const userToDelete = await this.userService.findById(id);
+
+    if (!userToDelete) {
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
     }
 
-    @Put('/update')
-    async updateUser(@Req() req: Request, @Body() userDTO: UserDTO): Promise<UserDTO> {
-        const userFoundById = await this.userService.findById(userDTO.id);
-
-        if (!userFoundById) {
-            throw new HttpException('User not found', HttpStatus.NOT_FOUND);
-        }
-
-        // const userReq = req.user?.login;
-        // userDTO.lastModifiedBy = userReq;
-
-        const userUpdated = await this.userService.update(userDTO);
-
-        return userUpdated;
-    }
-
-    @Delete('/delete/:id')
-    async deleteUser(@Param('id') id: number): Promise<UserDTO> {
-        const userToDelete = await this.userService.findById(id);
-
-        if (!userToDelete) {
-            throw new HttpException('User not found', HttpStatus.NOT_FOUND);
-        }
-
-        return await this.userService.delete(userToDelete);
-    }
-
+    return await this.userService.delete(userToDelete);
+  }
 }
